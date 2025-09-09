8c427f9e2e3573c3ee1522b956f695d7539b8b86
RyanVerWey <ryan.w.verwey@gmail.com>
2025-09-09 19:58:28 -0400

    init

--- typingspeedtest/src/components/layout/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <footer className="w-full mt-8 text-center text-xs opacity-60 py-6">
-      Built with React, Tailwind, and ❤️
-    </footer>
-  );
-}
